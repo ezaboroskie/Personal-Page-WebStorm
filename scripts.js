4a03e31e168cba13c346a6f19b21409f7b22b047
Eddie Zaboroskie <ezaboroskie@MacBook-Pro-2.local>
2023-10-04 19:20:39 -0400

    adds header link click events to jump to sections

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 const backgroundVid = document.getElementById('background-video')
-
+const span1 = document.getElementById('span1');
+const span2 = document.getElementById('span2');
+const span3 = document.getElementById('span3');
+const contactSection = document.getElementById('contact-section')
+const topHidden = document.getElementById('top-hidden')
+const servicesSection = document.getElementById('services-section')
 backgroundVid.playbackRate = .5
+
+span1.addEventListener('click', function(event) {
+    topHidden.scrollIntoView()
+});
+span2.addEventListener('click', function(event) {
+    servicesSection.scrollIntoView()
+});
+span3.addEventListener('click', function(event) {
+    contactSection.scrollIntoView()
+});
+
+
 
 $(document).ready(function(){
     $('#header-span-container span').on({
@@ -11,4 +28,6 @@ $(document).ready(function(){
             $(this).removeClass('active')
         }
     })
+
 })
+
