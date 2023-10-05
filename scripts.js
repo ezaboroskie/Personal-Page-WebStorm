aedc78ba766fde439f8d031564734ca53787b794
Eddie Zaboroskie <ezaboroskie@MacBook-Pro-2.ftsbninc.local>
2023-10-05 12:20:04 -0400

    adds media breakpoints and responsive styling

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@ const span3 = document.getElementById('span3');
 const contactSection = document.getElementById('contact-section')
 const topHidden = document.getElementById('top-hidden')
 const servicesSection = document.getElementById('services-section')
+const p1 = document.getElementById('p1')
+const p2 = document.getElementById('p2')
+const p3 = document.getElementById('p3')
+
 backgroundVid.playbackRate = .5
 
 span1.addEventListener('click', function(event) {
@@ -16,7 +20,15 @@ span2.addEventListener('click', function(event) {
 span3.addEventListener('click', function(event) {
     contactSection.scrollIntoView()
 });
-
+p1.addEventListener('click', function(event) {
+    topHidden.scrollIntoView()
+});
+p2.addEventListener('click', function(event) {
+    servicesSection.scrollIntoView()
+});
+p3.addEventListener('click', function(event) {
+    contactSection.scrollIntoView()
+});
 
 
 $(document).ready(function(){
@@ -28,6 +40,18 @@ $(document).ready(function(){
             $(this).removeClass('active')
         }
     })
+    $('#dropdown-menu').hide()
+    $("#hamburger-icon").click(function(e){
+        e.stopPropagation(); // Prevent the click event from propagating to the document
+        $("#dropdown-menu").toggle();
+    });
+
+    // Close the dropdown menu when a click occurs outside the menu and the hamburger icon
+    $(document).click(function(e) {
+        if(!$(e.target).closest('#dropdown-menu').length && !$(e.target).is('#hamburger-icon')) {
+            $('#dropdown-menu').hide();
+        }
+    });
 
 })
 
